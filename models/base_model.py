2e5f2009b8efbed7af07147ae628c74f3ae3d920
nokeefe <nokeefe1919@gmail.com>
2019-06-27 20:43:42 +0000

    added test and models folders with barebones basemodel class file

---
+++ models/base_model.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" base class defining all common attributes
+    and methods; to be inherited from. Finished
+    console will interact with class with restrictions
+"""
+#import json
+
+
+class BaseModel:
+    """ function is base class/file for  """
