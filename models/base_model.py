b2ed4c61b02c86e886d638a62994c02a17275e1f
Cyn <cmdominguez@gmail.com>
2019-06-28 04:04:16 +0000

    adding str and dict repr

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 
 import json
 import uuid
-from datetime import datetime, date, time
+from datetime import datetime
 
 
 class BaseModel():
@@ -14,17 +14,27 @@ class BaseModel():
         Arg1: variable id is initialized at None
         '''
         self.id =  uuid.uuid4()
-        self.created_at = day.today()
-        self.updated_at = day.today()
         ''' id is an attribute of the class
             created_at is datetime, assigns when instance created
             updated_at is datetime as above and updates on object change
-            '''
+        '''
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.now()
         ''' uuid4 function generates random unique user id '''
-        """
-        print ("string representation of uuid4() are : ")
-        print (repr(id.str))
-        print ("byte representation : ")
-        print (repr(id.bytes))
-        str(id)
-        """
+        self.__dict__ = self.dict
+        self.__class__ = self.dict
+
+    def __str__(self):
+        ''' String representation of class name, id, and __dict__ '''
+        return ('[{}] ({}) {}'.format(self, self.id, self.dict))
+
+    def save(self):
+        ''' Instance that updates updated_at with current datetime '''
+        self.updated_at = datetime.now(tz=None)
+
+    def to_dict(self):
+        ''' Returns dictionary representation of keys/values '''
+        '''
+        return {'my_number': , 'name': , 'updated_at': self.updated_at,
+                'id': self.id, 'created_at': self.created_at}
+        '''
