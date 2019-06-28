f30f8e17ba6a656571f63a5bee995c9804bb79db
nokeefe <nokeefe1919@gmail.com>
2019-06-28 00:04:46 +0000

    uuid generated random id string and working on datetime

--- models/base_model.py
+++ models/base_model.py
@@ -3,28 +3,28 @@
 
 import json
 import uuid
+from datetime import datetime, date, time
 
 
 class BaseModel():
     ''' Parent class/file for future classes in separate files '''
-    def __init__(self, id=None):
+    def __init__(self):
         '''
         Creates instance of BaseModel
         Arg1: variable id is initialized at None
         '''
-        self.id = id
-        self.created_at = created_at
-        self.updated_at = updated_at
+        self.id =  uuid.uuid4()
+        self.created_at = day.today()
+        self.updated_at = day.today()
         ''' id is an attribute of the class
             created_at is datetime, assigns when instance created
             updated_at is datetime as above and updates on object change
             '''
-        id = uuid.uuid4()
         ''' uuid4 function generates random unique user id '''
+        """
         print ("string representation of uuid4() are : ")
         print (repr(id.str))
         print ("byte representation : ")
         print (repr(id.bytes))
-        """
         str(id)
         """
