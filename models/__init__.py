61d1bb92f08f2d388fd92d97ae65185700a83984
nokeefe <nokeefe1919@gmail.com>
2019-06-29 21:14:06 +0000

    serialization

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+#import engine.file_storage import FileStorage
+
+#super().__init__(self)
+#    self.storage = storage
---
+++ models/engine/file_storage.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+''' Module for FileStorage '''
+
+import json
+import models.base_model import BaseModel
+
+
+class FileStorage():
+''' class serializes instances to JSON file and
+    deserializes JSON file to instances
+'''
+    def __init__(self):
+    '''Creates instance of FileStorage'''
+        __file_path = 'file.json'
+        __objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+
+    def save(self):
+        with open("file.json", "w") as serialized_file:
+            json.dump(obj, serialized_file)
+
+
+    def reload(self):
+
+    else do nothing
